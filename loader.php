2b3046c47297d219724f8059dc1b62f574c49389
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-03-05 13:40:43 +0100

    Disabled normal classloading in Core package and added all classes to loader object.

--- loader.php
+++ loader.php
@@ -24,7 +24,20 @@ $env->dic->set_classes(array(
 // Forge and return the Core Package object
 return $env->forge('Loader:Package')
 	->set_path(__DIR__)
-	->set_namespace('Fuel\\Core')
+	->set_namespace(false)
+	->add_classes(array(
+		'Fuel\\Core\\Controller\\Template' => __DIR__.'/classes/Controller/Template.php',
+		'Fuel\\Core\\Loader\\Closure' => __DIR__.'/classes/Loader/Closure.php',
+		'Fuel\\Core\\Loader\\Lowercase' => __DIR__.'/classes/Loader/Lowercase.php',
+		'Fuel\\Core\\Parser\\Markdown' => __DIR__.'/classes/Parser/Markdown.php',
+		'Fuel\\Core\\Parser\\Twig' => __DIR__.'/classes/Parser/Twig.php',
+		'Fuel\\Core\\Presenter\\Base' => __DIR__.'/classes/Presenter/Base.php',
+		'Fuel\\Core\\Request\\Curl' => __DIR__.'/classes/Request/Curl.php',
+		'Fuel\\Core\\Security\\String\\Xss' => __DIR__.'/classes/Security/String/Xss.php',
+		'Fuel\\Core\\Debug' => __DIR__.'/classes/Debug.php',
+		'Fuel\\Core\\Error' => __DIR__.'/classes/Error.php',
+		'Fuel\\Core\\Profiler' => __DIR__.'/classes/Profiler.php',
+	))
 	->add_class_aliases(array(
 		'Classes\\Controller\\Template'  => 'Fuel\\Core\\Controller\\Template',
 		'Classes\\Presenter\\Base'       => 'Fuel\\Core\\Presenter\\Base',
